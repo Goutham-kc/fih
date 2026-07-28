3675b2a218547906aa483ffc34712598ec58a115
Goutham K C <gouthamkc153@gmail.com>
2026-07-28 19:13:27 +0530

    feat: add support for custom logo.png / favicon in public folder

--- pages/_app.js
+++ pages/_app.js
@@ -6,11 +6,10 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/logo.png" />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#6c63ff" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+        <meta name="theme-color" content="#121319" />
       </Head>
       <Component {...pageProps} />
     </>
